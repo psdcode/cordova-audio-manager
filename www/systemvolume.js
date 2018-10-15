712e07c73068db04fb951583663e57456cb0300c
Felipe Augusto <felipe.nunes.silva@outlook.com>
2018-10-15 14:48:34 -0300

    Adding methods to module.exports to allow the user to access them

--- www/systemvolume.js
+++ www/systemvolume.js
@@ -1,8 +1,22 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-
     setSystemVolume: function(volume) {
         exec(null, null, "SystemVolume", "setSystemVolume", [volume]);
+    },
+    setVoiceCallVolume: function(volume) {
+        exec(null, null, "SystemVolume", "setVoiceCallVolume", [volume]);
+    },
+    setRingVolume: function(volume) {
+        exec(null, null, "SystemVolume", "setRingVolume", [volume]);
+    },
+    setAlarmVolume: function(volume) {
+        exec(null, null, "SystemVolume", "setAlarmVolume", [volume]);
+    },
+    setMusicVolume: function(volume) {
+        exec(null, null, "SystemVolume", "setMusicVolume", [volume]);
+    },
+    setAccessibilityVolume: function(volume) {
+        exec(null, null, "SystemVolume", "setAccessibilityVolume", [volume]);
     },
 };
